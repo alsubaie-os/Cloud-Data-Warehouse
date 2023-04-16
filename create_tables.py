cfc1b690976e145ff3ba90f57054f25604e572a2
“Osama <osama.s.alsubaie@gmail.com>
2023-04-16 16:48:26 +0300

    Add docstrings

--- create_tables.py
+++ create_tables.py
@@ -1,14 +1,18 @@
+# This python file is used to create your fact and dimension tables for the star schema in Redshift. 
+# You can run this file to reset your tables before each time you run your ETL scripts. 
+# You can also add in this functionality to your ETL scripts to create the tables in Redshift before you run your ETL scripts to insert data.
+
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
+# This function drops all tables in the Redshift database if they exist
 def drop_tables(cur, conn):
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+# This function creates the tables in the Redshift database
 def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
--- etl.py
+++ etl.py
@@ -1,14 +1,16 @@
+# This python file is used to load data from S3 into staging tables on Redshift and then process that data into your analytics tables on Redshift. 
+
 import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
+# This function loads data from S3 into staging tables on Redshift
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
-
+# This function processes data from staging tables into analytics tables on Redshift
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
